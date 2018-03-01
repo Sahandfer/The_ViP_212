1c4732c886e16f686f1423000fabd6b77d696d71
Sahand Sabour <35892312+Sahandfer@users.noreply.github.com>
2018-03-01 20:57:19 +0800

    Update WeBuddy-JAVASCRIPT.js

--- WeBuddy-JAVASCRIPT.js
+++ WeBuddy-JAVASCRIPT.js
@@ -1,8 +1,14 @@
 window.onload= function(){
     var n=0;
+    var totalDistance= 0;
+    var initialLongtitude=0;
+    var initialLatitute=0;
+    var currentLongtitude=0;
+    var currentLatitute=0;
     setInterval(timerTim,1000);
     getLocation();
     setInterval(getLocation2,5000);
+    setInterval(distanceWalked,5000);
     }
 function sayHello(){
     var z= document.getElementById("Bye");
@@ -20,13 +26,18 @@ function getLocation(){
 
 }
 
+
+
 function showFirstPosition(position){
-    var initialLongtitude=position.coords.longitude;
-    var initialLatitute=position.coords.latitude;
+    initialLongtitude=position.coords.longitude;
+    initialLatitute=position.coords.latitude;
     var x= document.getElementById("Location");
     x.innerHTML = "Beginning Latitude: " + initialLatitute + 
         "<br>Beginning Longitude: " + initialLongtitude;
 }
+
+
+
 function getLocation2(){
     var x= document.getElementById("Location");
     
@@ -35,13 +46,19 @@ function getLocation2(){
     } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }}
+
+
+
 function showSecondPosition(position){
-    var initialLongtitude=position.coords.longitude;
-    var initialLatitute=position.coords.latitude;
+    currentLongtitude=position.coords.longitude;
+    currentLatitute=position.coords.latitude;
     var x= document.getElementById("Location2");
-    x.innerHTML = "Current Latitude: " + initialLatitute + 
-        "<br>Current Longitude: " + initialLongtitude;
+    x.innerHTML = "Current Latitude: " + currentLatitute + 
+        "<br>Current Longitude: " + currentLongtitude;
+    
 }
+
+
 
 function timerTim(){
     var date= new Date();
@@ -51,7 +68,24 @@ function timerTim(){
 
 
 
+function distanceWalked(){
+var mapIndicate= document.getElementById("map");
     
-    
+var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(currentLatitute-initialLatitute);  // deg2rad below
+  var dLon = deg2rad(currentLongtitude-initialLongtitude); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(initialLatitute)) * Math.cos(deg2rad(currentLatitute)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c * 1000; // Distance in meters.
+    totalDistance += d;
+    mapIndicate.innerHTML="Distance walked: "+totalDistance +" meters!";
+}
 
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180);
+}
