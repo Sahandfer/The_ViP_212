56c096270e01bef3cad69dab37ea2c5073484642
Sahand Sabour <35892312+Sahandfer@users.noreply.github.com>
2018-03-01 20:59:01 +0800

    Update WeBuddy-JAVASCRIPT.js

--- WeBuddy-JAVASCRIPT.js
+++ WeBuddy-JAVASCRIPT.js
@@ -80,8 +80,8 @@ var R = 6371; // Radius of the earth in km
     Math.sin(dLon/2) * Math.sin(dLon/2)
     ; 
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c * 1000; // Distance in meters.
-    totalDistance += d;
+  var distance = R * c * 1000; // Distance in meters.
+    totalDistance = distance + totalDistance;
     mapIndicate.innerHTML="Distance walked: "+totalDistance +" meters!";
 }
 
